20ba5cee2dba68a3754b5e65d630e01aaed8b7b9
tituspasta <76658643+tituspasta@users.noreply.github.com>
2020-12-30 15:44:47 +0100

    Create archsetup.sh

---
+++ archsetup.sh
@@ -0,0 +1,6 @@
+pacman -S xorg-server xorg-xinit xorg-xset xorg-xsetroot xclip alacritty noto-fonts rofi neovim lxsession &&
+bash setup.sh 
+#setting up yay
+#cd /tmp && git clone https://aur.archlinux.org/yay-bin.git &&
+#cd yay-bin && makepkg -si &&
+#yay -S brave-bin

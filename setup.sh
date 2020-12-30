b27d68b6d949490ba9037f71aa5a0b5ec1ed11b3
tituspasta <76658643+tituspasta@users.noreply.github.com>
2020-12-30 15:39:24 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,3 @@
-#mega sync
-#yay -S curl &&
-#curl --output mega.pkg.tar.zst https://mega.nz/linux/MEGAsync/Arch_Extra/x86_64/megasync-x86_64.pkg.tar.zst
-#yay -U --noconfirm mega.pkg.tar.zst && rm -rf mega.pkg.zst &&
-yay -S xorg-server xorg-xinit xclip alacritty noto-fonts rofi xorg-xsetroot brave-bin lxsession &&
 sudo cp dwm /usr/local/bin/dwm &&
 sudo chmod 755 /usr/local/bin/dwm &&
 cp .bashrc ~/
